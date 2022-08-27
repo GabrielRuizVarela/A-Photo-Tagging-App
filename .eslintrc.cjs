166de82e1060b077d3dc81e03bd64f6b645ebf30
Gabriel Ruiz Varela <gabrielruizvarela@gmail.com>
2022-08-27 18:32:16 -0300

    Initial configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx', '.jsx'] }],
+    'import/extensions': [0, 'never'],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
