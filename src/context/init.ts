9584bd03eafa8d31ef107620eb18e4e46c0a8857
Gabriel Ruiz Varela <gabrielruizvarela@gmail.com>
2022-08-28 18:27:12 -0300

    Updated initial props

--- src/context/init.ts
+++ src/context/init.ts
@@ -4,9 +4,13 @@ import image1 from '../assets/pierre-roussel-wii-phone2.jpg';
 const ImagesContext = createContext([
   {
     image: image1,
+    alt: 'Nintendo theme level',
     id: 1,
     dimensions: { width: 1080, height: 2340 },
-    targets: [{ x: 208, y: 714 }, { x: 405, y: 509 }],
+    targets: [
+      { x: 208, y: 714 },
+      { x: 405, y: 509 },
+    ],
   },
 ]);
 
